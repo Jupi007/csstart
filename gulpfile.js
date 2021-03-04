776f47e1e05f087023d04c40e184b3d1e480d2f9
Jupiter007 <paul.kepinski@orange.fr>
2021-03-04 11:34:27 +0100

    Update packages - use dart-sass instead of node-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,52 @@
 var gulp = require('gulp');
+var sass = require('gulp-dart-sass');
 var p = require('gulp-load-plugins')();
 var autoprefixer = require('autoprefixer');
 var csso = require('gulp-csso');
 var browserSync = require('browser-sync').create();
 
-gulp.task('css', function(){
-	return gulp.src('src/sass/csstart.scss')
+gulp.task('css', function() {
+    return gulp.src('src/sass/csstart.scss')
 
-	.pipe(p.sourcemaps.init())
-		.pipe(p.sass())
-		.pipe(p.csscomb())
-		.pipe(p.cssbeautify({indent: '	'}))
-		.pipe(p.postcss([
-			autoprefixer()
-		]))
-	.pipe(p.sourcemaps.write("./"))
+        .pipe(p.sourcemaps.init())
+        .pipe(sass())
+        .pipe(p.csscomb())
+        .pipe(p.cssbeautify({
+            indent: '	'
+        }))
+        .pipe(p.postcss([
+            autoprefixer()
+        ]))
+        .pipe(p.sourcemaps.write("./"))
 
-	.pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('css-min', function(){
-	return gulp.src('src/sass/csstart.scss')
+gulp.task('css-min', function() {
+    return gulp.src('src/sass/csstart.scss')
 
-	.pipe(p.sass())
-	.pipe(p.csscomb())
-	.pipe(p.cssbeautify({indent: '	'}))
-	.pipe(p.postcss([
-		autoprefixer()
-	]))
-    .pipe(csso())
+        .pipe(sass())
+        .pipe(p.csscomb())
+        .pipe(p.cssbeautify({
+            indent: '	'
+        }))
+        .pipe(p.postcss([
+            autoprefixer()
+        ]))
+        .pipe(csso())
 
-	.pipe(p.rename({
-      suffix: '.min'
-    }))
-	.pipe(gulp.dest('build/css'));
+        .pipe(p.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('watch',  function() {
-	browserSync.init({
-		proxy: {
-	        target: "localhost"
-	    }
-	});
+gulp.task('watch', function() {
+    browserSync.init({
+        proxy: {
+            target: "localhost"
+        }
+    });
     gulp.watch('src/sass/**/*.scss', gulp.series('default')).on('change', browserSync.reload);
 });
 
